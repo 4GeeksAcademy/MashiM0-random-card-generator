10f11952247f5af1a9b14616af7d94ab2f404b97
MashiM0 <massimog.boschettib@gmail.com>
2025-02-20 17:27:50 +0000

    [new: create card component and randomizer script]

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/favicon.ico";
+
+function randomNumber(arr){
+  return Math.floor(Math.random()*arr.length)
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ['♦', '♥', '♠', '♣']
+  const ranks = ['A', '1', '2', '3','4', '5', '6', '7', '8', '9', 'J','Q','K']
+  const rankIds = ['heart', 'spades', 'clubs', 'diamonds']
+
+    let cardSuits = document.querySelectorAll(".suit")
+    let suit = randomNumber(suits)
+      for(let cardSuit of cardSuits){
+        cardSuit.innerHTML = suits[suit]
+      }
+
+    if(suit in ['♦', '♥']){
+      document.querySelector(".black").classList.add("red")
+      document.querySelector(".black").classList.remove("black")
+    }
+
+    let cardRanks = document.querySelector(".rank")
+    let rank = randomNumber(ranks)
+      cardRanks.innerHTML = ranks[rank]
 };
